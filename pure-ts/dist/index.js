d5ce858b0e5333a3ba279df25b7c925b213d76e4
intikhab-h-bhat <44907729+intikhab-h-bhat@users.noreply.github.com>
2025-03-05 14:07:04 +0530

    classes

--- pure-ts/dist/index.js
+++ pure-ts/dist/index.js
@@ -1,2 +1,22 @@
 "use strict";
-console.log("This is typescript here. lets learn it");
+// class User{
+//     name: string;
+//     age: number;
+//     private city: string='Bangalore';
+//     constructor(abc:string,xyz:number){
+//         this.name = abc;
+//         this.age = xyz;
+//     }
+// }
+// const User1=new User('John',25);
+// //User1.city='Mumbai';
+// console.log(User1);
+class User {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+        this.city = 'Bangalore';
+    }
+}
+const user = new User('John', 25);
+console.log(user);
--- pure-ts/src/index.ts
+++ pure-ts/src/index.ts
@@ -1,1 +1,30 @@
-console.log("This is typescript here. lets learn it")
+
+// class User{
+//     name: string;
+//     age: number;
+//     private city: string='Bangalore';
+//     constructor(abc:string,xyz:number){
+//         this.name = abc;
+//         this.age = xyz;
+//     }
+// }
+
+// const User1=new User('John',25);
+// //User1.city='Mumbai';
+// console.log(User1);
+
+class User{
+
+    readonly city:string='Bangalore';
+    constructor(
+        public name:string,
+        public age:number,
+       )
+        {
+           
+        }
+        
+}
+
+const user=new User('John',25);
+console.log(user);
